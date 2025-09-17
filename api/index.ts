9c0cffd40ed36a9163c5021b750d97ad00aa47a3
kimshun0213kr <96727208+kimshun0213kr@users.noreply.github.com>
2025-09-17 22:27:30 +0900

    Update: Render deploy用に更新

--- api/index.ts
+++ api/index.ts
@@ -12,10 +12,17 @@ const app = express();
 // );
 setupMiddleware(app);
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/health", (req, res) => res.sendStatus(200));
 app.post("/test", (req, res) => {
   console.log(req.body);
 });
 app.use("/watchdog", watchdog);
 app.use("/v1", v1);
+
+const port = process.env.PORT || 3006;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log("Server listening on 3006!");
+});
 
 export default app;
